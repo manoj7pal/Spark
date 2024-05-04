9f545c73c20cb266438845f31ab20c7b3f258a3d
Manoj Pal <manoj7pal@gmail.com>
2024-05-04 19:26:05 -0400

    Fixed env variables issues

--- week11/Spark_Dataframes/1.py
+++ week11/Spark_Dataframes/1.py
@@ -2,7 +2,8 @@ import os
 
 import findspark
 
-print(findspark.init('C:\spark\spark-2.4.4-bin-hadoop2.7'))
-
 spark_home = os.environ.get('SPARK_HOME', None)
-print(spark_home)
+java_home = os.environ.get('JAVA_HOME', None)
+
+print(f'Spark Home: {spark_home}')
+print(f'Java Home: {java_home}')
--- week11/Spark_Dataframes/example.py
+++ week11/Spark_Dataframes/example.py
@@ -1,3 +1,15 @@
+import os
+os.environ["JAVA_HOME"] = "C:/java/jdk-1.8"
+os.environ["SPARK_HOME"] = "C:/spark"
+import findspark
+findspark.init()
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master("local[*]").getOrCreate()
+df=spark.read.options(delimiter=",", header=True).csv("../../dataset/chapters.csv")
+df.show()
+
+
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
 
@@ -10,20 +22,23 @@ from pyspark import SparkConf
 import os
 
 spark_home = os.environ.get('SPARK_HOME', None)
-
-print(spark_home)
+java_home = os.environ.get('JAVA_HOME', None)
+print(f'Spark Home: {spark_home}')
+print(f'Java Home: {java_home}')
 
 # Spark Configuration
 sparkConf = SparkConf()
 sparkConf.set("spark.appName", "My First Application")
 sparkConf.set("spark.master", "local[*]")
+print('_____')
+
 
 # Spark Session
 spark = SparkSession.builder \
     .config(sparkConf) \
     .getOrCreate()
 
-# Logic here
+print(spark)
 
 
 # Close spark session
